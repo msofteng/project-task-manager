7cc38e9140a87018e7a0a4385c89b03905085ecb
msofteng <mathiaspedro9@gmail.com>
2024-12-15 18:10:58 -0300

    removendo comentários do bean do projeto

--- src/main/java/com/caed/projectaskmanager/bean/ProjetoBean.java
+++ src/main/java/com/caed/projectaskmanager/bean/ProjetoBean.java
@@ -10,13 +10,12 @@ import java.util.TimeZone;
 @Named("projetoBean")
 @RequestScoped
 public class ProjetoBean {
-    private Projeto projeto;  // O objeto que representa um projeto
-    private List<Projeto> projetos;  // A lista de projetos cadastrados
-
-    // Construtores
+    private Projeto projeto;
+    private List<Projeto> projetos;
+    
     public ProjetoBean() {
-        this.projeto = new Projeto();  // Inicializa o objeto de projeto
-        this.projetos = new ArrayList<>();  // Carrega a lista de projetos da base de dados
+        this.projeto = new Projeto();
+        this.projetos = new ArrayList<>();
         
         TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
     }
@@ -36,27 +35,24 @@ public class ProjetoBean {
     public void setProjetos(List<Projeto> projetos) {
         this.projetos = projetos;
     }
-
-    // Métodos de ação
+    
     public String salvarProjeto() {
-        // Chama o DAO para salvar o projeto na base de dados
         System.out.println("salvando o projeto");
-        System.out.println(this.projeto);
-        return "listagemProjetos.xhtml?faces-redirect=true";  // Redireciona para a página de listagem
+        return "listagemProjetos.xhtml?faces-redirect=true";
     }
 
     public String editarProjeto(Long id) {
         System.out.println("editando o projeto");
-        return "cadastroProjeto.xhtml?faces-redirect=true";  // Redireciona para a página de cadastro
+        return "cadastroProjeto.xhtml?faces-redirect=true";
     }
 
     public String excluirProjeto(Long id) {
         System.out.println("removendo o projeto");
-        return "listagemProjetos.xhtml?faces-redirect=true";  // Redireciona para a página de listagem
+        return "listagemProjetos.xhtml?faces-redirect=true";
     }
 
     public String visualizarProjeto(Long id) {
         System.out.println("visualizando o projeto");
-        return "visualizarProjeto.xhtml?faces-redirect=true";  // Exibe a página de visualização do projeto
+        return "visualizarProjeto.xhtml?faces-redirect=true";
     }
 }
